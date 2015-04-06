d3851e57762b024c4f9b4c5d7d9352af2e1dabe3
Ralph Bohnet <rbohnet@gmail.com>
2015-04-05 18:42:17 -0600

    Remove overriden methods for Gethub pages

--- _plugins/category_pages.rb
+++ _plugins/category_pages.rb
@@ -2,14 +2,6 @@
 # Adapted from https://github.com/runemadsen/runemadsen-2013 
 module Jekyll
 
-  # Override the default paginator
-  module Paginate
-    class Pagination < Generator
-      def generate(site)
-      end
-    end
-  end
-
   class CategoryPages < Generator
   
     safe true
@@ -30,7 +22,7 @@ module Jekyll
       category_posts = site.categories[category].sort_by { |p| -p.date.to_f }
 
       # calculate total number of pages
-      pages = CategoryPager.calculate_pages(category_posts, site.config['paginate'].to_i)
+      pages = CategoryPager.calculate_pages(category_posts, site.config['num_of_posts_to_paginate'].to_i)
 
       # iterate over the total number of pages and create a physical page for each
       (1..pages).each do |num_page|
@@ -56,7 +48,7 @@ module Jekyll
     attr_reader :category
 
     def self.pagination_enabled?(config, page)
-      page.name == 'index.html' && page.data['pagination_enabled'] && !config['paginate'].nil?
+      page.name == 'index.html' && page.data['pagination_enabled'] && !config['num_of_posts_to_paginate'].nil?
     end
 
     # Returns the pagination path as a string
@@ -70,7 +62,7 @@ module Jekyll
     def initialize(site, page, all_posts, category, num_pages = nil)
       @category = category
       @page = page
-      @per_page = site.config['paginate'].to_i
+      @per_page = site.config['num_of_posts_to_paginate'].to_i
       @total_pages = num_pages || Pager.calculate_pages(all_posts, @per_page)
 
       if @page > @total_pages
@@ -88,24 +80,14 @@ module Jekyll
       @next_page = @page != @total_pages ? @page + 1 : nil
       @next_page_path = paginate_path(site, @next_page, category)
     end
-
-    # use the original to_liquid method, but add in category info
-    alias_method :original_to_liquid, :to_liquid
-    def to_liquid
-      x = original_to_liquid
-      x['category'] = @category
-      x
-    end
   end
 
-  class Post 
+  class Jekyll::Post 
     # Allow navigation to next or previous post within the same category
-    alias_method :original_next, :next
-    alias_method :original_previous, :previous
 
-    def next
-      return original_next if self.categories.length != 1
-      category_posts = filter_by_category(self.categories[0])
+    def next_in_category
+      return self.next if self.categories.length != 1
+      category_posts = self.site.categories[self.categories[0]]
       pos = category_posts.index {|post| post.equal?(self)}
       if  pos && pos < category_posts.length - 1
            category_posts[pos + 1]
@@ -114,21 +96,15 @@ module Jekyll
       end
     end
 
-    def previous
-      return original_previous if self.categories.length != 1
-      category_posts = filter_by_category(self.categories[0]) 
+    def previous_in_category
+      return self.previous if self.categories.length != 1
+      category_posts = self.site.categories[self.categories[0]]
       pos = category_posts.index {|post| post.equal?(self)}
       if  pos && pos > 0 
            category_posts[pos - 1]
       else 
            nil
       end
-    end
-
-    protected
-
-    def filter_by_category(category)
-      site.categories[category].sort_by { |p| -p.date.to_f }      
     end
   end
 end
